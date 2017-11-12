b6f13d802828bfdc22b88649108b32f7f52c31f7
mdbaker1 <mb0165153@otc.edu>
2017-11-11 21:41:22 -0600

    completion of server handling of get requests

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname + '/about.html'));
+});
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname + '/contact.html'));
+});
+app.use("*", (req, res) => {
+    res.status(404).send('Page not found');
+});
+
+app.listen(3000, () => console.log('Server is running'));
